1482408661e1e45e0f77d2bf032d84e9290400c1
nelsW70 <nelsfoltz@gmail.com>
2018-05-29 12:17:51 -0400

    connect to firebase database

---
+++ src/components/AddGoal.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { goalRef } from '../firebase';
+
+class AddGoal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: ''
+    }
+  }
+
+  addGoal() {
+    console.log('this.state', this.state);
+    goalRef.push({email: 'test@test.com', title: this.state.title});
+  }
+
+  render() {
+    return (
+      <div className="form-inline">
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Add a goal"
+            className="form-control"
+            style={{marginRight: '5px'}}
+            onChange={event => this.setState({title: event.target.value})}
+          />
+          <button
+            className="btn btn-success"
+            type="button"
+            onClick={() => this.addGoal()}
+          >
+            Submit
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default AddGoal;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { firebaseApp } from '../firebase';
+import AddGoal from './AddGoal';
 
 class App extends Component {
+  signOut() {
+    firebaseApp.auth().signOut();
+  }
+
   render() {
     return (
-      <div>App</div>
+      <div>
+        <h3>Goals</h3>
+        <AddGoal/>
+        <div>Goal List</div>
+          <button
+            className="btn btn-danger"
+            onClick={() => this.signOut()}
+            >
+              Sign Out
+          </button>
+      </div>
     )
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  console.log('state', state);
+  return {}
+}
+
+export default connect(mapStateToProps, null)(App);
